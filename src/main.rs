8936044848f33c2eccf2bc5a893c55d19411d162
Arvid Sätterkvist <arvidsat@gmail.com>
2020-07-01 23:05:12 +0200

    feat: working example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use specs::prelude::*;
-use std::time::Duration;
 use std::net::TcpListener;
 use std::thread;
+use std::time::Duration;
 use tungstenite::server::accept;
-
-// A component contains data which is associated with an entity.
 
 #[derive(Debug)]
 struct Vel(f32);
@@ -23,16 +21,9 @@ impl Component for Pos {
 struct SysA;
 
 impl<'a> System<'a> for SysA {
-    // These are the resources required for execution.
-    // You can also define a struct and `#[derive(SystemData)]`,
-    // see the `full` example.
     type SystemData = (WriteStorage<'a, Pos>, ReadStorage<'a, Vel>);
 
     fn run(&mut self, (mut pos, vel): Self::SystemData) {
-        // The `.join()` combines multiple components,
-        // so we only access those entities which have
-        // both of them.
-        // You could also use `par_join()` to get a rayon `ParallelIterator`.
         for (pos, vel) in (&mut pos, &vel).join() {
             pos.0 += vel.0;
             println!("position: {:?}", pos);
@@ -41,21 +32,10 @@ impl<'a> System<'a> for SysA {
 }
 
 fn main() {
-    // The `World` is our
-    // container for components
-    // and other resources.
-
     let mut world = World::new();
 
-    // This builds a dispatcher.
-    // The third parameter of `add` specifies
-    // logical dependencies on other systems.
-    // Since we only have one, we don't depend on anything.
-    // See the `full` example for dependencies.
     let mut dispatcher = DispatcherBuilder::new().with(SysA, "sys_a", &[]).build();
 
-    // setup() must be called before creating any entity, it will register
-    // all Components and Resources that Systems depend on
     dispatcher.setup(&mut world);
 
     world.create_entity().with(Vel(2.0)).with(Pos(0.0)).build();
@@ -63,24 +43,26 @@ fn main() {
     world.create_entity().with(Vel(1.5)).with(Pos(5.4)).build();
 
     let server = TcpListener::bind("127.0.0.1:3000").unwrap();
-    for stream in server.incoming() {
-        thread::spawn (move || {
-            let mut websocket = accept(stream.unwrap()).unwrap();
-            loop {
-                let msg = websocket.read_message().unwrap();
 
-                // We do not want to send back ping/pong messages.
-                if msg.is_binary() || msg.is_text() {
-                    println!("Message: {}", msg);
-                    websocket.write_message(msg).unwrap();
-                }
-            }
-        });
-    }
-
-    // This dispatches all the systems in parallel (but blocking).
+    server.set_nonblocking(true).unwrap();
     loop {
         dispatcher.dispatch(&world);
+
+        // Accept all tcp connections before moving on
+        while let Ok((stream, _)) = server.accept() {
+            stream.set_nonblocking(false).unwrap();
+            thread::spawn(move || {
+                let mut websocket = accept(stream).unwrap();
+                loop {
+                    let msg = websocket.read_message().unwrap();
+
+                    if msg.is_binary() || msg.is_text() {
+                        println!("Message: {}", msg);
+                        websocket.write_message(msg).unwrap();
+                    }
+                }
+            });
+        }
 
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 1)); // 1 fps
     }
